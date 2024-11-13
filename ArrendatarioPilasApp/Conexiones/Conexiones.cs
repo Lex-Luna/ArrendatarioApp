258cf7264640b05625bc51b5781f1950f6f4a1cd
Lex Luna <alexisgabriel.lu@gmail.com>
2024-11-13 15:38:19 -0500

    agrega los script necesarios para crear cadena de conexion

---
+++ ArrendatarioPilasApp/Conexiones/Conexiones.cs
@@ -0,0 +1,16 @@
+﻿using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrendatarioPilasApp.Conexiones
+{
+    public class Conexiones
+    {
+        public static FirebaseClient firebase = new FirebaseClient("https://arrendatariopilas-default-rtdb.firebaseio.com/");
+        public const string WebapyFirebase = "\r\nAIzaSyCPoJ-mAfk_u74Q2TzIx6u6QtYhQSqBZBs";
+        //public static string storage = "huecasapp-d8da1.appspot.com";
+
+        //public const string GoogleMapsApiKey = "AIzaSyDkbzFWQC_jmqn6NY1_F92aOLVfoKbrRr8";
+    }
+}
